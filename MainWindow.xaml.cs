bbced7d38734aed36a962f9975133337fc987d22
LucasAdao <l.adao@outlook.com>
2024-01-17 18:52:52 -0300

    Project Update

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using ApiCep.models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ApiCep
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+        Main();
+        async Task Main()
+        {
+            string cepinho = textBoxCep.Text;
+            string apiUrl = $"https://api.invertexto.com/v1/cep/{cepinho}?token=6152|AxrTMQPh4Y5id5bG8WdAQUrU0KHHvxFf"
+;
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseBody = await response.Content.ReadAsStringAsync();
+
+                        // Converte a resposta JSON para um objeto C#
+                        Cep cep = JsonConvert.DeserializeObject<Cep>(responseBody);
+
+                        // Agora você pode trabalhar com o objeto C# conforme necessário
+                        textBoxBairro.Text = cep.Bairro;
+                        textBoxCep.Text = cep.CepNumero;
+                        textBoxCidade.Text = cep.Cidade;
+                        textBoxEstado.Text = cep.Estado;
+                        textBoxRua.Text = cep.Rua;
+                        
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Erro na requisição: {response.StatusCode} - {response.ReasonPhrase}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Erro: {ex.Message}");
+                }
+            }
+        }
+
+        }
+
+    }
+    }
+
---
+++ models/Cep.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiCep.models
+{
+    public class Cep
+    {
+        [JsonProperty("cep")]
+        public string CepNumero{ get; set; }
+        [JsonProperty("state")]
+        public string Estado{ get; set; }
+        [JsonProperty("city")]
+        public string Cidade { get; set; }
+        [JsonProperty("neighborhood")]
+        public string Bairro { get; set; }
+        [JsonProperty("street")]
+        public string Rua {  get; set; }
+    }
+}
